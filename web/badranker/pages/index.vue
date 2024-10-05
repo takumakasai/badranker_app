8436c15c7f8a0fdc59bb33327945629018870a1e
t_kasai <wphnr827@yahoo.co.jp>
2024-10-05 16:00:30 +0900

    fix: デプロイテスト用に簡素なソースに変更

--- web/badranker/pages/index.vue
+++ web/badranker/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-sheet class="mx-auto semi-transparent" width="300" min-height="20em">
+  <p>pppppp</p>
+  <!-- <v-sheet class="mx-auto semi-transparent" width="300" min-height="20em">
     <v-form fast-fail @submit.prevent>
       <v-text-field v-model="userId" label="ID" />
       <v-text-field v-model="password" label="パスワード" />
@@ -8,33 +9,33 @@
     <div class="text-center mt-4">
       <v-btn prepend-icon="mdi mdi-account" color="primary" elevation="2" outlined plain @click="$router.push('user_new')">新規アカウント</v-btn>
     </div>
-  </v-sheet>
+  </v-sheet> -->
 </template>
 
 <script setup lang="ts">
 
-  const router = useRouter()
-  const loginUser = useLoginUser()
+  // const router = useRouter()
+  // const loginUser = useLoginUser()
 
-  const userId = ref('2') // FIXME: テスト用に初期値で2をセット
-  const password = ref('')
+  // const userId = ref('2') // FIXME: テスト用に初期値で2をセット
+  // const password = ref('')
 
-  const login = async () => {
-    try {
-      const res = await useApi('users/' + userId.value)
-      // ログイン情報をステートにセット
-      loginUser.value.id = res.value.payload.id
-      loginUser.value.name = res.value.payload.name
-      loginUser.value.email = res.value.payload.email
-      loginUser.value.rank = res.value.payload.rank
-      loginUser.value.status = res.value.payload.status
+  // const login = async () => {
+  //   try {
+  //     const res = await useApi('users/' + userId.value)
+  //     // ログイン情報をステートにセット
+  //     loginUser.value.id = res.value.payload.id
+  //     loginUser.value.name = res.value.payload.name
+  //     loginUser.value.email = res.value.payload.email
+  //     loginUser.value.rank = res.value.payload.rank
+  //     loginUser.value.status = res.value.payload.status
 
-      router.push('rank')
+  //     router.push('rank')
 
-    } catch (error) {
-      console.error('Error occurred:', error)
-    }
-  }
+  //   } catch (error) {
+  //     console.error('Error occurred:', error)
+  //   }
+  // }
 
 </script>
 
