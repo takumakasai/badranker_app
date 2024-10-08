d8d7a6aea08b82b19c9452145baa160a3bf0d5c4
t_kasai <wphnr827@yahoo.co.jp>
2024-10-08 19:33:11 +0900

    fix

---
+++ api/bin/bash/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# サーバー起動前に古いPIDファイルを削除
+rm -f /badranker/tmp/pids/server.pid
+
+# データベースのマイグレーションを実行
+bundle exec rails db:migrate
+
+# Railsサーバーを起動
+bundle exec rails s -b 0.0.0.0 -p 10001
--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # 許可するホスト名を追加
+  config.hosts << "badranker-app-api.onrender.com"
 end
