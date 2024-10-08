1126bbd2b732a19c00e6373dfbe6d89a608e3e45
t_kasai <wphnr827@yahoo.co.jp>
2024-10-09 01:46:16 +0900

    fix

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,10 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3001',
-            'badranker-app.onrender.com'
+            'badranker-app.onrender.com',
+            'badranker-app-api.onrender.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head],
-      credentials: true
+      # credentials: true
+      credentials: false
   end
 end
